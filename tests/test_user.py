3a3e833c557260ee391d739e87e6725dca93115c
Андрей Никитин <146591318+AndrewNikkk@users.noreply.github.com>
2025-04-18 00:01:27 +0300

    first commit

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from src.main import app
+from src.fake_db.database import db
 
 client = TestClient(app)
 
@@ -15,6 +16,11 @@ users = [
         'id': 2,
         'name': 'Petr Petrov',
         'email': 'p.p.petrov@mail.com',
+    },
+    {
+        'id': 3,
+        'name': 'Vladimir Vladimirov',
+        'email': 'v.v.vladimirov@mail.com',
     }
 ]
 
@@ -26,16 +32,31 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'non_existent_email'})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+    
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    response = client.post("/api/v1/user", json=users[2])
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
+    
+    user_in_db = db.get_user_by_email(users[2]["email"])
+    assert user_in_db is not None
+    assert user_in_db["name"] == users[2]["name"]
+    assert user_in_db["email"] == users[2]["email"]
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    response = client.post("/api/v1/user", json=users[1])
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    response = client.delete("/api/v1/user", params={'email': users[2]['email']})
+    assert response.status_code == 204
+    assert db.get_user_by_email(users[2]['email']) is None
+    
